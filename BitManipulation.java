c932a92c540a8c64f3ad32a438eb10025e6cce03
sarfraaz1428 <sarfraaz14282007111@gmail.com>
2026-09-07 22:04:38 +0530

    bitmanipulation

---
+++ BitManipulation.java
@@ -0,0 +1,15 @@
+public class BitManipulation{
+  public static void main(String[] args) {
+    int n=5;
+    int pos =2;
+    int bitmask = 1<<pos;
+
+    if((bitmask & n)== 0){
+      System.out.println("bit was Zero");
+
+    }
+    else{
+      System.out.println("bit was one");
+    }
+  }
+}
